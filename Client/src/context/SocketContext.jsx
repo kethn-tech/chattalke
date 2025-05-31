dd69a364876413f322727ffff750d62651d58b25
Kethan Ayatti <kethanayatti333@gmail.com>
2025-05-31 14:27:24 +0530

    done

--- Client/src/context/SocketContext.jsx
+++ Client/src/context/SocketContext.jsx
@@ -22,7 +22,7 @@ export const SocketProvider = ({ children }) => {
         query: {
           userId: userInfo._id,
         },
-        transports: ["websocket", "polling"], // Ensure compatibility with different environments
+        transports: ["websocket"], // Ensure compatibility with different environments
       });
 
       socket.current.on("connect", () => {
